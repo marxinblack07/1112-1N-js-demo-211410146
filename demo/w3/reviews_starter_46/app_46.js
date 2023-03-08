e202d239e2877623b64c2c5df1c302fbd5ef0d4c
marxinblack07 <211410146@o365.tku.edu.twprintf>
2023-03-08 16:18:28 +0800

    W03-p3: Implement next button and show the sequence in console log

--- demo/w3/reviews_starter_46/app_46.js
+++ demo/w3/reviews_starter_46/app_46.js
@@ -48,11 +48,24 @@ const randomBtn = document.querySelector('.random-btn');
 
 
 let currentItem = 0;
+
 window.addEventListener('DOMContentLoaded',() => {
-  const item = reviews[currentItem];
-  img.src = item.img;
+  showPreview(currentItem);
+});
+
+const showPreview = (person) => {
+  const item = reviews[person];
+  console.log('item',item);
+  item.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
-  console.log('item',item);
-});
+}
+
+nextBtn.addEventListener('click', () => {
+  currentItem++;
+  if(currentItem >= reviews.length) {
+    currentItem = 0;
+  }
+  showPreview(currentItem);
+})
